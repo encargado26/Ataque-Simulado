eca6dc11979b7e64d7785fd181c3d3e4449641f5
encargado26 <caceresparedmarceloagustin@gmail.com>
2026-09-16 15:52:27 -0300

    modifique mi test de buque

--- tests/Buque.test.ts
+++ tests/Buque.test.ts
@@ -13,6 +13,13 @@ describe("Buque", () => {
 		expect(buque.estaVivo()).toBe(true);
 	});
 
+	it("debería resistir dos disparos y seguir vivo", () => {
+    	const buque = new Buque();
+    	buque.recibirDisparo(1);
+    	buque.recibirDisparo(1);
+    	expect(buque.estaVivo()).toBe(true);
+	});
+
 	it("se hunde al perder sus 3 vidas", () => {
 		const buque = new Buque();
 		buque.recibirDisparo(1);
@@ -20,4 +27,9 @@ describe("Buque", () => {
 		buque.recibirDisparo(1);
 		expect(buque.estaVivo()).toBe(false);
 	});
+
+	it("debería poder disparar correctamente", () => {
+    const buque = new Buque();
+    expect(() => buque.disparar()).not.toThrow();
+  });
 });
